c106d8ad34e94f9900fe84469b32275ea16c7167
NicolaRicci100 <nicola.ricci09@gmail.com>
2023-06-09 22:33:42 +0200

    aggiunta pulsante di gioco e condizioni vittoria

--- js/script.js
+++ js/script.js
@@ -23,9 +23,13 @@ const row = document.getElementById('main-row');
 //variabile per il timer
 const timer = document.getElementById('timer');
 
+//bottone di gioco
+const playButton = document.getElementById('play');
+
 //creo un array per la lista di numeri
 const listNumbers = [];
-
+//creo un array per vedere se i numeri alla fine saranno giusti
+const isValidNUmber = [];
 //creo i div per i numeri
 for (let i = 0; i < 5; i++) { //giro 5 volte
     const randomNumber = random(1, 99, listNumbers); //genero il numero random da 1 a 99
@@ -33,18 +37,32 @@ for (let i = 0; i < 5; i++) { //giro 5 volte
     const box = buildBox(); //inserisco il contenitore per il numero
     box.innerText = randomNumber; //inserisco il numero nel contenitore
     row.appendChild(box); //inserisco le col nella row
+    console.log(listNumbers);
 }
 
-let seconds = 10; //durata del timer
+let seconds = 5; //durata del timer
 timer.innerText = seconds; //mostro il timer
 const counter = setInterval(() =>{
     timer.innerText = --seconds; //il tempo scorre
     if(seconds === 0){ //allo 0...
         clearInterval(counter); //il timer si ferma
         timer.innerText = 'TEMPO SCADUTO'
-        row.className = 'text-white'; //i numeri spariscono
-        for (let i = 0; i < 5; i++){
-            let respond = prompt('scrivi uno dei numeri che hai visto');
-        }
+        row.innerHTML = ''; //i numeri spariscono
+        playButton.classList.remove('d-none'); //posso vedere il bottone
+        document.getElementById("play").addEventListener("click", function(){ //se premo il bottone posso giocare
+            for (let i = 0; i < 5; i++){
+                let respond = prompt('scrivi uno dei numeri che hai visto');
+                console.log(respond);
+                isValidNUmber.push(respond); //carico i numeri nell'array
+                console.log(isValidNUmber);
+            }
+            if (isValidNUmber.toString() === listNumbers.toString()){ //comparo gli array dei numeri dell'utente con quelli casuali
+                timer.innerText = 'HAI VINTO! hai azzeccato tutti i numeri'; //se azzecchi tutti i numeri
+                playButton.className = ('d-none'); //rimuovo il bottone
+            } else{
+                timer.innerText = 'HAI PERSO! i numeri erano '+ listNumbers; //se sbagli almeno un numero
+                playButton.className = ('d-none'); //rimuovo il bottone
+            }
+        })
     }
 }, 1000);
