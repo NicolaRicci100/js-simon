82760b882ca40d8c76983ecb17e43ee21bba828e
NicolaRicci100 <nicola.ricci09@gmail.com>
2023-06-09 16:11:51 +0200

    creazione funzione random

--- js/script.js
+++ js/script.js
@@ -1,1 +1,14 @@
 console.log('JS OK');
+
+//* FUNZIONE RANDOM
+// min = numero minimo | max = numero massimo | numbers = quantità di numeri
+random = (min, max, numbers) => {
+    const totNumbers = []; //array vuoto per ospitare i numeri casuali
+    for (i = 0; i < numbers; i++){
+        randomNumber = Math.floor(Math.random() * (max - min + 1) ) + min; //randomizzo da un minimo ad un massimo
+        while (totNumbers.includes(randomNumber)); //mi assicuro di non avere doppioni
+        totNumbers.push(randomNumber); //quindi inserisco nell'array
+    }
+    return totNumbers; //restituisco l'array completo
+}
+console.log(random(1, 99, 5));
